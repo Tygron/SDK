daa6d69bfc66600a632a0edfa11b205d4b56dc2d
Rudolf Koster <Tygron@users.noreply.github.com>
2015-05-01 10:59:29 +0200

    Added ability to store credentials for the examples in a separate file, ignored by the repo.

--- src/com/tygron/pub/examples/settings/ExampleSettings.java
+++ src/com/tygron/pub/examples/settings/ExampleSettings.java
@@ -1,17 +1,50 @@
 package com.tygron.pub.examples.settings;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
- * This class provides settings for the included examples
+ * This class provides credentials for the examples. It is also possible to create the file
+ * "authentication.properties" in this package, with the following contents, which will overwrite the values
+ * here:<br>
+ * USERNAME=youraccount@tygron.com<br>
+ * PASSWORD=yourpassw0rd<br>
+ * SERVER=https://server2.tygron.com:3020/<br>
  * @author Rudolf
  *
  */
 
 public class ExampleSettings {
-	public final static String SERVER = "https://server2.tygron.com:3020/";
 
-	public final static String USERNAME = "";
-	public final static String PASSWORD = "";
+	public static String SERVER = "https://server2.tygron.com:3020/";
 
-	public final static String CLIENT_ADDRESS = "Unknown IP address";
-	public final static String CLIENT_NAME = "Example from SDK";
+	public static String USERNAME = "";
+
+	public static String PASSWORD = "";
+	public static String CLIENT_ADDRESS = "Unknown IP address";
+
+	public static String CLIENT_NAME = "Example from SDK";
+
+	static {
+		if (USERNAME == null || USERNAME.equals("") || PASSWORD == null || PASSWORD.equals("")
+				|| SERVER == null || SERVER.equals("")) {
+			try {
+
+				Properties prop = new Properties();
+				InputStream in = ExampleSettings.class.getResourceAsStream("authentication.properties");
+				prop.load(in);
+				in.close();
+
+				USERNAME = prop.getProperty("USERNAME", USERNAME);
+				PASSWORD = prop.getProperty("PASSWORD", PASSWORD);
+				SERVER = prop.getProperty("SERVER", SERVER);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.err
+						.println("Failed to load credentials file: authentication.properties, and no credentials in ExampleSettings.");
+				System.exit(-1);
+			}
+		}
+	}
 }
