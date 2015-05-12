a7309b82bc1f04d96f19d8b28597077d501adc69
Orbitton <orbitton@gmail.com>
2015-05-12 13:50:31 +0200

    Additional logging function

--- src/com/tygron/pub/logger/Log.java
+++ src/com/tygron/pub/logger/Log.java
@@ -4,20 +4,8 @@ public class Log {
 
 	private static boolean verbose = false;
 
-	public static void setVerbose(boolean verbose) {
-		Log.verbose = verbose;
-	}
-
-	private static void out(String message) {
-		System.out.println(message);
-	}
-
 	private static void err(String message) {
 		System.err.println(message);
-	}
-
-	public static void info(String message) {
-		out(message);
 	}
 
 	public static void exception(Throwable t, String message) {
@@ -25,7 +13,25 @@ public class Log {
 		t.printStackTrace();
 	}
 
+	public static void info(String message) {
+		out(message);
+	}
+
+	private static void out(String message) {
+		System.out.println(message);
+	}
+
+	public static void setVerbose(boolean verbose) {
+		Log.verbose = verbose;
+	}
+
 	public static void verbose(String message) {
-		if (verbose) out(message);
+		if (verbose) {
+			out(message);
+		}
+	}
+
+	public static void warning(String message) {
+		err(message);
 	}
 }
