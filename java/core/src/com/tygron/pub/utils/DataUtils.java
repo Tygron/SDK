bc3b008fad59d30cbeb43427f4f9ecf5732c3fe1
Orbitton <orbitton@gmail.com>
2015-05-18 16:18:54 +0200

    Removed now deprecated methods

--- java/core/src/com/tygron/pub/utils/DataUtils.java
+++ java/core/src/com/tygron/pub/utils/DataUtils.java
@@ -1,6 +1,5 @@
 package com.tygron.pub.utils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,81 +15,6 @@ import com.tygron.pub.logger.Log;
  *
  */
 public class DataUtils {
-
-	/**
-	 * For use when retrieving data sets directly from a session.
-	 * @param list The list to collapse.
-	 * @return The collapsed list.
-	 */
-	@Deprecated
-	public static List<Object> collapseMapsInList(List<? extends Object> list) {
-		if (list == null) {
-			return null;
-		}
-		List<Object> returnable = new ArrayList<Object>();
-
-		for (Object o : list) {
-			if (!(o instanceof Map)) {
-				returnable.add(0);
-				continue;
-			}
-
-			Map firstChild = (Map) o;
-			if (firstChild.size() != 1) {
-				returnable.add(firstChild);
-				continue;
-			}
-
-			// If there's only one child in the currently selected map in the list
-			for (Object subChild : firstChild.values()) {
-				returnable.add(subChild);
-			}
-		}
-		return returnable;
-	}
-
-	/**
-	 * For use when data sets are pulled via update poll.
-	 * @param map the map to collapse.
-	 * @return The collapsed maps.
-	 */
-	@Deprecated
-	public static Map<Object, Object> collapseMapsInListsInMapsInMap(
-			Map<? extends Object, ? extends Object> map) {
-		if (map == null) {
-			return null;
-		}
-		// input = items
-		Map<Object, Object> returnable = new HashMap<Object, Object>();
-
-		for (Entry e : map.entrySet()) {
-			// entry = item : itembucket (which is a map).
-			if (!(e.getValue() instanceof Map)) {
-				returnable.put(e.getKey(), e.getValue());
-				continue;
-			}
-			Map firstChild = (Map) e.getValue();
-
-			// firstchild=itembucket (which is a map).
-			if (firstChild.size() != 1) {
-				returnable.put(e.getKey(), e.getValue());
-				continue;
-			}
-
-			// If there's only one child in the currently selected bucket (which is likely list).
-			returnable.put(e.getKey(), new ArrayList<Object>()); // The item's bucket is now a list.
-			for (Object subChild : firstChild.values()) { // Subchild is the original bucket's content.
-				if (!(subChild instanceof List)) {
-					returnable.put(e.getKey(), subChild); // Not a list, so place it directly in the bucket.
-					continue;
-				}
-				returnable.put(e.getKey(), collapseMapsInList((List) subChild)); // It is a list, so collapse
-																					// it further.
-			}
-		}
-
-		return returnable;
-	}
 
 	/**
 	 * Collapse a map returned by the long-polling update mechanic of the API into a standard format.
@@ -156,7 +80,6 @@ public class DataUtils {
 	 * @return Map&lt;String, Map&lt;Integer, Map&lt;?, ?&gt;&gt;&gt;, where the String key is a MapLink, the
 	 *         Integer is an itemID and the niclosed map represent a single item of data.
 	 */
-
 	public static Map<Integer, Map<?, ?>> dataListToMap(List<Map<String, Map<?, ?>>> list) {
 		if (list == null) {
 			return null;
